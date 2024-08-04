4e7ba7405faa32e356e0bdda7dec5a336ac74836
Gulrez Alam <egulrezalam@gmail.com>
2024-08-05 02:04:13 +0530

    Update Firebase configuration to use environment variables

--- firebase.js
+++ firebase.js
@@ -8,15 +8,15 @@ import { getDatabase, getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWcAbWzOdFodWLiN9Rpil5hBaFmBJlAEo",
-  authDomain: "pantry-trackerg.firebaseapp.com",
-  databaseURL: "https://pantry-trackerg-default-rtdb.firebaseio.com",
-  projectId: "pantry-trackerg",
-  storageBucket: "pantry-trackerg.appspot.com",
-  messagingSenderId: "741344411462",
-  appId: "1:741344411462:web:bc731621b2b542ce37ef47",
-  measurementId: "G-QXG0S3YW3H"
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
